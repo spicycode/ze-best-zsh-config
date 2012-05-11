eeb348372e57e3615178ed4b988b7d9889ef2180
Chad Humphries <chad.humphries@gmail.com>
2012-05-11 11:45:26 -0700

    Removing unused vim aliases, adding in hitch.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,11 +6,8 @@ alias ll='ls -GFhl'
 
 alias ri='ri -Tf ansi'
 
-alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
-alias vi='/Applications/MacVim.app/Contents/MacOS/Vim'
-alias gvim='mvim -p'
-
 alias mysql='mysql -u root'
 alias mysqladmin='mysqladmin -u root'
+
 alias e='aquamacs'
 alias be='bundle exec'

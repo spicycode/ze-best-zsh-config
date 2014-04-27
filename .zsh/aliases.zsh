0212ed26d286954539217061b6bc04829d9d91e2
Chad Humphries <chad.humphries@gmail.com>
2014-04-27 14:40:25 -0400

    Simplify gentags

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -14,7 +14,7 @@ alias bx='bundle exec'
 
 alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
 
-alias gentags='ctags . && rm .tags;mv tags .tags'
+alias gentags='ctags .'
 alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
 alias rake='noglob rake'
 alias bower='noglob bower'

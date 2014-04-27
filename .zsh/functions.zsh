9cea0d96471f506be8d172c852a82cfed0dcb27a
Chad Humphries <chad.humphries@gmail.com>
2014-04-27 14:40:40 -0400

    Make 'enter' with no command run git status

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -72,3 +72,18 @@ function sc {
 function fix_airplay {
   sudo kill `ps -ax | grep 'coreaudiod' | grep 'sbin' |awk '{print $1}'`
 }
+
+# Detect empty enter, execute git status if in git dir
+magic-enter () {
+        if [[ -z $BUFFER ]]; then
+          if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+            echo -ne '\n'
+            git status 
+          fi
+          zle accept-line
+        else
+          zle accept-line
+        fi
+}
+zle -N magic-enter
+bindkey "^M" magic-enter

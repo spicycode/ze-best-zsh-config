8fc8da6cd64499436c3c0791e6ce3d4159d11718
Chad Humphries <chad.humphries@gmail.com>
2014-07-20 11:33:21 -0400

    Use percol for history search when present

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -87,3 +87,19 @@ magic-enter () {
 }
 zle -N magic-enter
 bindkey "^M" magic-enter
+
+function exists { which $1 &> /dev/null }
+
+if exists percol; then
+    function percol_select_history() {
+        local tac
+        exists gtac && tac="gtac" || { exists tac && tac="tac" || { tac="tail -r" } }
+        BUFFER=$(fc -l -n 1 | eval $tac | percol --query "$LBUFFER")
+        CURSOR=$#BUFFER         # move cursor
+        zle -R -c               # refresh
+    }
+
+    zle -N percol_select_history
+    bindkey '^R' percol_select_history
+fi
+

8c647831a5fb6eed18d22dfbda449432a82a6fea
Chad Humphries <chad.humphries@gmail.com>
2015-06-05 09:36:30 -0400

    Drop unused aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,11 +4,8 @@ alias ls='ls -GFh'
 # Same as above, but in long listing format
 alias ll='ls -GFhl'
 
-alias ri='ri -Tf ansi'
-
 alias mysql='mysql -u root'
 alias mysqladmin='mysqladmin -u root'
 
 alias rake='noglob rake'
 alias bower='noglob bower'
-alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'

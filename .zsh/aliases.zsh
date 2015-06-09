bc795f29e1aef3740d803d9207e0c241f58a6171
Chad Humphries <chad.humphries@gmail.com>
2015-06-09 10:15:19 -0400

    Dropping unused mysql aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,8 +4,5 @@ alias ls='ls -GFh'
 # Same as above, but in long listing format
 alias ll='ls -GFhl'
 
-alias mysql='mysql -u root'
-alias mysqladmin='mysqladmin -u root'
-
 alias rake='noglob rake'
 alias bower='noglob bower'

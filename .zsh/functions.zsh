a674aa99a790ffcc0f5b6a38da0b5bf5a94c6681
Chad Humphries <chad.humphries@gmail.com>
2015-07-26 11:28:12 -0400

    Drop magic-enter, it's fallen out of use

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -52,18 +52,3 @@ function sc {
     script/rails console $@
   fi
 }
-
-# Detect empty enter, execute git status if in git dir
-magic-enter () {
-        if [[ -z $BUFFER ]]; then
-          if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-            echo -ne '\n'
-            git status 
-          fi
-          zle accept-line
-        else
-          zle accept-line
-        fi
-}
-zle -N magic-enter
-bindkey "^M" magic-enter

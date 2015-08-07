23e7a4cf8068f534eea5ace617dd43c06dd61f9e
Chad Humphries <chad.humphries@gmail.com>
2015-08-07 08:02:55 -0400

    Add trash function

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -52,3 +52,19 @@ function sc {
     script/rails console $@
   fi
 }
+
+function trash () {
+  local path
+  for path in "$@"; do
+    # ignore any arguments
+    if [[ "$path" = -* ]]; then :
+    else
+      local dst=${path##*/}
+      # append the time if necessary
+      while [ -e ~/.Trash/"$dst" ]; do
+        dst="$dst "$(date +%H-%M-%S)
+      done
+      /bin/mv "$path" ~/.Trash/"$dst"
+    fi
+  done
+}
